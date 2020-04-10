c21c15a3a49870ed5bcd7a2375dfac8e6bdea279
Ben Marwick <benmarwick@hotmail.com>
2020-04-10 14:12:08 -0700

    Citation

---
+++ analysis/paper/functions-for-gender.R
@@ -0,0 +1,117 @@
+# computer gender by first name
+
+# read in data
+
+get_the_gender_data <- function(x){
+
+  d <- x
+
+library(tidyverse)
+library(readxl)
+
+total_sessions_in_conf <- max(d$session) # Look in the PDF
+
+# clean up exotic symbols
+# from http://stackoverflow.com/a/17517674/1036500
+fromto <- read.table(text="
+                     from to
+                     Å¡ s
+                     Å oe
+                     Å¾ z
+                     Ã ss
+                     Ã¾ y
+                     Ã th
+                     Ã  a
+                     Ã¡ a
+                     Ã¢ a
+                     Ã£ a
+                     Ã¤ a
+                     Ã¥ a
+                     Ã¦ ae
+                     Ã§ c
+                     Ã¨ e
+                     Ã© e
+                     Ãª e
+                     Ã« e
+                     Ã E
+                     Ã¬ i
+                     Ã� i
+                     Ã® i
+                     Ã¯ i
+                     Ã° d
+                     Ã± n
+                     Ã² o
+                     Ã³ o
+                     Ã´ o
+                     Ãµ o
+                     Ã¶ o
+                     Ã¸ oe
+                     Ã¹ u
+                     Ãº u
+                     Ã» u
+                     Ã¼ u
+                     Ã½ y
+                     Ã¿ y
+                     Ä g
+                     ",header=TRUE)
+# Then the function:
+
+replace_foreign_chars <- function(dat,fromto) {
+  for(i in 1:nrow(fromto) ) {
+    dat <- gsub(fromto$from[i],fromto$to[i],dat)
+  }
+  dat
+}
+
+# tidy up accents and foreign characters
+d$`first-named speaker` <- replace_foreign_chars(d$FirstName, fromto)
+
+# extract first name
+require("Hmisc")
+library(purrr)
+first_word_safe <- safely(first.word)
+
+d$first_name <-
+  map(d$FirstName, first_word_safe) %>%
+  map(., c(1)) %>%
+  flatten_chr()
+
+# fill down on session numbers because I only numbered the first speaker in each session.
+library(tidyr)
+d <- fill(d, session)
+
+# remove dupicate names in each session
+d <-
+  d %>%
+  group_by(session) %>%
+  distinct(FirstName, .keep_all = TRUE )
+
+# compute gender of first name
+library(gender)
+library(genderdata)
+library(dplyr)
+
+# make a safe function so we get a result even if it's an error
+gender_safe <-  safely(gender)
+
+# identify the gender of each first nane, this may take a few moments...
+d_genders <-
+  d  %>%
+  # hold out name col to use it to make list-col
+  nest_legacy(-session) %>%
+  # compute genders and store results in list-col
+  mutate(first_name_gender = map(data,
+                                 ~gender_safe(.x$`first-named speaker`)))
+
+# Ignoring error list, just looking at the result list with gender
+d_genders_unnest <-
+  d_genders %>%
+  mutate(speaker_gender = transpose(first_name_gender)[['result']]) %>%
+  unnest_legacy(speaker_gender) %>%
+  select(session, gender, name)
+
+return(genders_unnest = d_genders_unnest)
+
+}
+
+
